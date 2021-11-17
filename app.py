fe1eb348e88bfb88bc60a695f4e6a84d8fcf7324
Claudio Ramos <claudio@dinclas.dev>
2021-11-17 10:01:23 -0300

    feature: authenticate, trust endpoints

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
-from flask import Flask, Response
+from flask import Flask, Response, request, jsonify
 from pynuinvest import NuInvest
 import os
 app = Flask(__name__)
 
 nu = NuInvest(solverr_url=os.environ["SOLVERR_URL"])
 
-if ('NUINVEST_EASYTOKEN' in os.environ):
-    nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'], os.environ['NUINVEST_DEVICE_ID'],
-                    os.environ['NUINVEST_EASYTOKEN'])
-    nu.trust_device(os.environ['NUINVEST_DEVICE_ID'])
-else:
-    nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'], os.environ['NUINVEST_DEVICE_ID'])
+try:
+    if ('NUINVEST_EASYTOKEN' in os.environ):
+        nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'], os.environ['NUINVEST_DEVICE_ID'],
+                        os.environ['NUINVEST_EASYTOKEN'])
+    else:
+        nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'], os.environ['NUINVEST_DEVICE_ID'])
+    print('Authenticated')
+except Exception as e:
+    print('Failed to authenticate: ' + str(e))
 
 
 @app.route('/metrics')
@@ -24,6 +27,20 @@ def metrics():  # put application's code here
         result += "net_value{{investment=\"{}\"}} {}\n".format(invesment["nickName"], invesment["netValue"])
 
     return Response(result, mimetype='text/plain')
+
+@app.route('/authenticate', methods=['POST'])
+def authenticate():
+    payload = request.json
+    nu.authenticate(os.environ['NUINVEST_USERNAME'],
+                    os.environ['NUINVEST_PASSWORD'],
+                    os.environ['NUINVEST_DEVICE_ID'],
+                    payload['otp'])
+
+    return jsonify({ "result": "ok", "otp": payload['otp'] })
+
+@app.route('/trust')
+def trust_device():
+    nu.trust_device(os.environ['NUINVEST_DEVICE_ID'])
 
 if __name__ == '__main__':
     app.run()
