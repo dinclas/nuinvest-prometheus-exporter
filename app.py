393dbb19bec1b230d5aa16a07006402012fbc1aa
Claudio Ramos <claudio@dinclas.dev>
2021-11-17 09:31:54 -0300

    feature: trust device on login

--- app.py
+++ app.py
@@ -4,7 +4,14 @@ import os
 app = Flask(__name__)
 
 nu = NuInvest(solverr_url=os.environ["SOLVERR_URL"])
-nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'], os.environ['NUINVEST_DEVICE_ID'], os.environ['NUINVEST_EASYTOKEN'])
+
+if ('NUINVEST_EASYTOKEN' in os.environ):
+    nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'], os.environ['NUINVEST_DEVICE_ID'],
+                    os.environ['NUINVEST_EASYTOKEN'])
+    nu.trust_device(os.environ['NUINVEST_DEVICE_ID'])
+else:
+    nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'], os.environ['NUINVEST_DEVICE_ID'])
+
 
 @app.route('/metrics')
 def metrics():  # put application's code here
