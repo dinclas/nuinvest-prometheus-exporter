cf61abb8ca9fd61cff59767fe9bb2edbd7c7b936
Claudio Ramos <claudio@dinclas.dev>
2021-11-19 13:48:41 -0300

    fix to only sum fundos, added fundos totals, added relogin

--- app.py
+++ app.py
@@ -1,30 +1,52 @@
+import sys
+
 from flask import Flask, Response, request, jsonify
 from pynuinvest import NuInvest
 import os
+import time
+
 app = Flask(__name__)
 
 nu = NuInvest(solverr_url=os.environ["SOLVERR_URL"])
 
-try:
-    if ('NUINVEST_EASYTOKEN' in os.environ):
-        nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'], os.environ['NUINVEST_DEVICE_ID'],
-                        os.environ['NUINVEST_EASYTOKEN'])
-    else:
-        nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'], os.environ['NUINVEST_DEVICE_ID'])
-    print('Authenticated')
-except Exception as e:
-    print('Failed to authenticate: ' + str(e))
+last_login = None
 
+def login():
+    global last_login
+    if last_login and time.time() - last_login < 60 * 60:
+        print('already logged')
+        return
+
+    try:
+        if ('NUINVEST_EASYTOKEN' in os.environ):
+            nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'],
+                            os.environ['NUINVEST_DEVICE_ID'],
+                            os.environ['NUINVEST_EASYTOKEN'])
+        else:
+            nu.authenticate(os.environ['NUINVEST_USERNAME'], os.environ['NUINVEST_PASSWORD'],
+                            os.environ['NUINVEST_DEVICE_ID'])
+        last_login = time.time()
+        print('Authenticated')
+    except Exception as e:
+        print('Failed to authenticate: ' + str(e))
 
 @app.route('/metrics')
 def metrics():
+    login()
+
     investments = nu.get_investment_data()
     result = ''
     result += "total_balance {}\n".format(investments['totalBalance'])
 
+    invested_in_fundos = 0
+    total_in_fundos = 0
+
     for invesment in investments['investments']:
-        if invesment['settlement']:
+        if invesment['settlement'] or invesment['investmentType']['description'] != 'Fundos de Investimento':
             continue
+
+        invested_in_fundos += invesment['investedCapital']
+        total_in_fundos += invesment['netValue']
 
         investment_percentage = invesment['investedCapital'] / investments['totalBalance']
         investment_roe = (invesment["netValue"] - invesment["investedCapital"]) / invesment["investedCapital"] * 100
@@ -33,7 +55,11 @@ def metrics():
         result += "net_value{{investment=\"{}\"}} {}\n".format(invesment["nickName"], invesment["netValue"])
         result += "index{{investment=\"{}\"}} {}\n".format(invesment["nickName"], investment_index)
 
+    result += "invested_in_fundos {}\n".format(invested_in_fundos)
+    result += "total_in_fundos {}\n".format(total_in_fundos)
+
     return Response(result, mimetype='text/plain')
+
 
 @app.route('/authenticate', methods=['POST'])
 def authenticate():
@@ -43,11 +69,13 @@ def authenticate():
                     os.environ['NUINVEST_DEVICE_ID'],
                     payload['otp'])
 
-    return jsonify({ "result": "ok", "otp": payload['otp'] })
+    return jsonify({"result": "ok", "otp": payload['otp']})
+
 
 @app.route('/trust')
 def trust_device():
     nu.trust_device(os.environ['NUINVEST_DEVICE_ID'])
+
 
 if __name__ == '__main__':
     app.run()
